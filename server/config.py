c3313255f8650cd7e9da07a33ffe02970067eab3
Aspen Wilson <aspen.schell@gmail.com>
2023-09-27 15:54:55 -0700

    Trying to get db deployed

--- server/config.py
+++ server/config.py
@@ -1,5 +1,5 @@
 # Standard library imports
-
+import os
 # Remote library imports
 from flask import Flask
 from flask_cors import CORS
@@ -14,7 +14,8 @@ from flask_bcrypt import Bcrypt
 # Instantiate app, set attributes
 app = Flask(__name__)
 app.secret_key = b'\x0f\xf1\xd6\xd4\x8fS0m\xad\xb2t\x97\xa5\x0eAc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
