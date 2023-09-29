dc91e775f659db05653d33518018feb2e4bff5bd
Aspen Wilson <aspen.schell@gmail.com>
2023-09-29 11:11:53 -0700

    Back to getting backend to run locally

--- server/app.py
+++ server/app.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 # Remote library imports
-from flask import render_template
+# from flask import render_template
 from config import app
 from routes.routes import *
 from models.models import *
 
-@app.route('/')
-@app.route('/<int:id>')
-def index(id=0):
-    return render_template("index.html")
+# @app.route('/')
+# @app.route('/<int:id>')
+# def index(id=0):
+#     return render_template("index.html")
 
 
 if __name__ == '__main__':
--- server/config.py
+++ server/config.py
@@ -11,32 +11,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-app = Flask(
-    __name__,
-    static_url_path='',
-    static_folder='../client/build',
-    template_folder='..client/build'
-    )
+app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
+app.secret_key = b'\x0f\xf1\xd6\xd4\x8fS0m\xad\xb2t\x97\xa5\x0eAc'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-app.secret_key = os.environ.get('SESSION_KEY')
-
-# Define metadata, instantiate db
-# metadata = MetaData(naming_convention={
-#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-# })
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
 
 bcrypt = Bcrypt(app)
+CORS(app)
 
-# Instantiate REST API
 api = Api(app)
 
-# Instantiate CORS
-CORS(app)
